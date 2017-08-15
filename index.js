d3ad13922da4267e747109f1e7da3f07096eb417
Bernard Strong <bstrong71@gmail.com>
2017-08-15 17:07:23 -0400

    basic styling and data grabbed

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
-
 const app = express();
+const data = require('./data');
+
+app.engine('mustache', mustacheExpress());
+app.set('views', './views');
+app.set('view engine', 'mustache');
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/users', function (req, res) {
+  res.render('index', { userList: data.users});
+});
+
+app.listen(3000, function() {
+  console.log("App is running on localhost:3000");
+})
